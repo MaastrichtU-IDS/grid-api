5b008efc5e2336953b0aa7edca5dfdfe9aa8a93e
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-22 09:02:59 +0100

    try to fix admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
-import { HydraAdmin } from '@api-platform/admin';
+import { HydraAdmin, ResourceGuesser } from '@api-platform/admin';
 
-export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
+// Full examplet at https://github.com/api-platform/demo/blob/master/admin/src/App.js
+
+export default () => (
+    <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}>
+        <ResourceGuesser name={"achievements"} />
+        <ResourceGuesser name={"sessions"} />
+    </HydraAdmin>);
