939226ddbf6b797e4f12b1f87e5abbe4c6c20c16
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-22 13:56:25 +0100

    add ListGuesser to fix admin UI

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { HydraAdmin, ResourceGuesser } from '@api-platform/admin';
-
 // Full examplet at https://github.com/api-platform/demo/blob/master/admin/src/App.js
+
+const AchievementList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"id"} />
+    </ListGuesser>
+);
+const SessionList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"id"} />
+    </ListGuesser>
+);
 
 export default () => (
     <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}>
-        <ResourceGuesser name={"achievements"} />
-        <ResourceGuesser name={"sessions"} />
+        <ResourceGuesser name={"achievements"} list={AchievementList} />
+        <ResourceGuesser name={"sessions"} list={SessionList}/>
     </HydraAdmin>);
